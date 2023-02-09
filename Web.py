3603fb8a02e85c505c74c8109f6537fc52012a84
Keanu-Type <keanuberchestype0@gmail.com>
2023-02-09 13:50:27 +0800

    web scraping image is get

--- Web.py
+++ Web.py
@@ -15,14 +15,31 @@ with open("Units_Alchemist_Code.csv", "r+", newline='') as csv_file:
 
         if unit[-1] == "NOT_SCRAPE":
             link=str(*unit[0:1]) #remove the []. without *. the output is "[LINK]" instead of "LINK"
-            link= "https://www.alchemistcodedb.com/unit/thol"+"#profile"
+            link+= "#profile"
             print(link)
             driver.get(link) #go to this website. 
             #driver.implicitly_wait(1)
             #rint(*unit[:2])
             #print(" ".join(unit))
             wait=WebDriverWait(driver,3).until(EC.presence_of_element_located((By.TAG_NAME, 'a')))
-            time.sleep(0.01)
+            try:
+                body = driver.find_element(By.ID, "profile")
+                image_set = body.find_elements(By.XPATH, './div/div[2]/div[2]/div')
+                for image in image_set:
+                    image = image.find_element(By.TAG_NAME, 'a').get_attribute('href')
+                    print(image)
+
+            except:
+                print("none")
+                break
+            #image_set = driver.find_elements(By.CLASS_NAME, "Card")
+            #print(image_set)
+            #for each_image in image_set:
+            #    Image_Link = each_image.find_element(By.TAG_NAME, 'a').get_attribute('href')
+            #    print(Image_Link)
+            #time.sleep(0.01)
+            
+            
             print("done")
             break
      #   else:
